eed47f306bb721c4bbc1c51cb76347f7b7f5d573
Jose Alves <josealves_w@yahoo.com.br>
2018-05-05 21:56:31 -0300

    projeto criado

---
+++ src/main/java/br/com/josealves/cursomc/cursomc/CursomcApplication.java
@@ -0,0 +1,12 @@
+package br.com.josealves.cursomc.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
